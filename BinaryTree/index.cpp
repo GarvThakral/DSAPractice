3584dfc95d90e51ef85b4d822bc1a061f8695576
Garv Thakral <garvthakralfx@gmail.com>
2025-02-17 02:16:42 +0530

    Create a binary tree

---
+++ BinaryTree/index.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+struct BinaryTree{
+    int data;
+    BinaryTree* left;
+    BinaryTree* right;
+    BinaryTree(int val){
+        data = val;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+
+
+void insertNode(BinaryTree*& head , int val){
+    if(head == nullptr){
+        BinaryTree* newNode = new BinaryTree(val);
+        head = newNode;
+        return;
+    }
+    if(head->data > val){
+        insertNode(head->left , val);
+    }
+    if(head->data < val){
+        insertNode(head->right , val);
+    }
+}
+
+void inorderTraversal(BinaryTree*& head){
+    if(head == nullptr){
+        return;
+    }
+    inorderTraversal(head->left);
+    cout << head->left->data;
+    cout << head->data;
+    cout << head->right->data;
+}
+
+int main(){
+    BinaryTree* head = nullptr;
+    insertNode(head,20);
+    insertNode(head,10);
+    insertNode(head,30);
+    insertNode(head,5);
+    insertNode(head,6);
+    cout << head->left->left->right->data;
+    return 0;
+}
