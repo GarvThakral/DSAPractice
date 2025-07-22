fc56f88eba66c5cae6fa3719a0bb88a86b8b12e8
Garv Thakral <garvthakralfx@gmail.com>
2025-07-22 15:26:03 +0530

    reversePairs

---
+++ StriverDSA/mergeSort.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int pairCount = 0;
+vector<vector<int>> pairVec;
+void merge(int low , int high , vector<int>& nums){
+    vector<int> temp;
+    int mid = (low+high)/2;
+    int i = low;
+    int j = mid+1;
+    int iter = mid+1;
+    while(i<=mid && j<=high){
+        if(nums[i] <= nums[j]){
+            temp.push_back(nums[i]);
+            i++;
+        }else{
+            temp.push_back(nums[j]);
+            j++;
+        }
+    }
+    while(i<=mid){
+        temp.push_back(nums[i]);
+        i++;
+    }
+    while(j<=high){
+        temp.push_back(nums[j]);
+        j++;
+    }
+    int index = 0;
+    for(i = low ; i<=high ;i++){
+        nums[i] = temp[index];
+        index++;
+    }
+}
+
+void mergeSort(int low , int high , vector<int>& nums){
+    if(low < high){
+        int mid = (low+high)/2;
+        mergeSort(low,mid,nums);
+        mergeSort(mid+1,high,nums);
+        merge(low , high , nums);
+    }
+}
+
+
+int main(){
+    vector<int> nums = {1,3,2,3,1};
+    int low = 0;
+    int high = nums.size()-1;
+    mergeSort(low  , high , nums);
+    cout << pairCount;
+}
