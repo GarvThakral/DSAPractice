df7ab05c97921dbceacd9b9b1ec1f1bd74ba6a43
Garv Thakral <garvthakralfx@gmail.com>
2025-09-08 00:32:28 +0530

    Optimal

--- StriverDSA/findPeak.cpp
+++ StriverDSA/findPeak.cpp
@@ -1,34 +1,36 @@
-class Solution {
-public:
-    bool isPeak(int l , int m , int r){
-        if(m > l && m > r){
-            return true;
-        }
-        return false;
-    }
-    int findPeakElement(vector<int>& nums) {
-        int low = 1;
-        int high = nums.size()-2;
-        int n = nums.size()-1;
-        if(nums.size() == 1){
-            return 0;
-        }
-        if(nums[n] > nums[n-1]){
-            return n;
-        }
-        if(nums[0] > nums[1]){
-            return 0;
-        }
-        while(low  <= high){
-            int mid = (low+high)/2;
-            if(isPeak(nums[mid-1],nums[mid],nums[mid+1])){
-                return mid;
-            }else if(nums[mid-1] < nums[mid]){
-                low = mid+1;
-            }else{
-                high = mid-1;
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> findPeakGrid(vector<vector<int>>& mat) {
+    int low = 0;
+    int high = mat[0].size()-1;
+    while(low <= high){
+        int mid = (low+high)/2;
+        int maxRow = 0;
+        int maxEl = 0;
+        for(int i = 0 ; i < mat.size() ;i++){
+            if(mat[i][mid] > maxEl){
+                maxRow = i;
+                maxEl = mat[i][mid];
             }
         }
-        return -1;
+        int left = mid - 1 > 0 ? mat[maxRow][mid-1]:-1;
+        int right = mid + 1 < mat[0].size() ? mat[maxRow][mid+1]:-1;
+        if(mat[maxRow][mid] > right && mat[maxRow][mid] > left){
+            return {maxRow , mid};
+        }else if(mat[maxRow][mid+1] > mat[maxRow][mid]){
+            low = mid+1;
+        }else{
+            high = mid - 1;
+        }
     }
-};
+}
+
+int main(){
+    vector<vector<int>> nums = {{1,4},{3,2}};
+    vector<int> ans = findPeakGrid(nums);
+    for(auto it:ans){
+        cout << it;
+    }
+}
