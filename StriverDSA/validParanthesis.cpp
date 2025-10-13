856775cd2fd127be1e821731f5326d217490f852
Garv Thakral <garvthakralfx@gmail.com>
2025-10-14 02:06:53 +0530

    optimal

--- StriverDSA/validParanthesis.cpp
+++ StriverDSA/validParanthesis.cpp
@@ -1,3 +1,59 @@
+// class Solution {
+// public:
+//     bool isValid(string paranthesis){
+//         stack<char> validPar;
+//         for(char c :paranthesis){
+//             if(c == ')' && validPar.empty()){
+//                 return 0;
+//             }
+//             if(c == '(' && validPar.empty()){
+//                 validPar.push(c);
+//                 continue;
+//             }
+//             if(c == '('){
+//                 validPar.push(c);
+//                 continue;
+//             }
+//             if(c == ')' && validPar.top() == '('){
+//                 validPar.pop();
+//                 continue;
+//             }
+//             if(c == ')'){
+//                 validPar.push(c);
+//             }
+//         }
+//             if(!validPar.empty()){
+//                 return 0;
+//             }
+//             return 1;
+//     };
+//     vector<string> glob;
+
+//     void generator(int n , int index , string temp){
+//         if(temp.size() == 2*n && isValid(temp)){
+//             glob.push_back(temp);
+//             return;
+//         }
+//         if(temp.size() > 2*n){
+//             return;
+//         }
+//         string temptemp = temp;
+//         temp += "(";
+
+//         generator(n,index+1,temp);
+//         temptemp += ")";
+
+//         generator(n,index+1,temptemp);
+//     } 
+    
+//     vector<string> generateParenthesis(int n) {
+//         string temp = "";
+//         glob.clear();
+//         generator(n,0,temp);
+//         return glob;
+//     }
+// };
+
 class Solution {
 public:
     bool isValid(string paranthesis){
@@ -29,27 +85,34 @@ public:
     };
     vector<string> glob;
 
-    void generator(int n , int index , string temp){
-        if(temp.size() == 2*n && isValid(temp)){
-            glob.push_back(temp);
+    void generator(int n , int index , string temp,int open,int close){
+        if(temp.size() == 2*n){
+            if(close != open){
+                return;
+            }
+            if(isValid(temp)){
+                glob.push_back(temp);
+            }
             return;
         }
         if(temp.size() > 2*n){
             return;
         }
         string temptemp = temp;
-        temp += "(";
-
-        generator(n,index+1,temp);
-        temptemp += ")";
-
-        generator(n,index+1,temptemp);
+        if(open<n){
+            temp += "(";
+            generator(n,index+1,temp,open+1,close);
+        }
+        if(close < open){
+            temptemp += ")";
+            generator(n,index+1,temptemp,open,close+1);
+        }
     } 
     
     vector<string> generateParenthesis(int n) {
         string temp = "";
         glob.clear();
-        generator(n,0,temp);
+        generator(n,0,temp,0,0);
         return glob;
     }
 };
