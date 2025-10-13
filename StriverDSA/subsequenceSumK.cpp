cd147b168921b51e4a8d5233a3601e413da60440
Garv Thakral <garvthakralfx@gmail.com>
2025-10-13 19:39:18 +0530

    subsequencesum

---
+++ StriverDSA/subsequenceSumK.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<vector<int>> glob;
+
+void checkSubsequenceSum(vector<int>& nums,int index, int target ,vector<int> temp) {
+    if(target == 0){
+        glob.push_back(temp);
+        return;
+    }
+    if(index == (nums.size()) || target < 0){
+        return;
+    }
+    temp.push_back(nums[index]);
+    checkSubsequenceSum(nums,index+1,target-nums[index],temp);
+    temp.pop_back();
+    checkSubsequenceSum(nums,index+1,target,temp);
+}
+
+
+int main(){
+    vector<int> nums = {1,2,3,4,5};
+    vector<int> temp;
+    checkSubsequenceSum(nums,0,8 ,temp);
+    for(auto vec:glob){
+        for(int item:vec){
+            cout << item << " ";
+        }
+        cout << endl;
+    }
+}
