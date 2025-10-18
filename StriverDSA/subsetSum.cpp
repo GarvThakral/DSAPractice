2d63deff82b81e0fe76ca21b9fa67325fe5f62ac
Garv Thakral <garvthakralfx@gmail.com>
2025-10-18 18:03:09 +0530

    Subset Sum II

--- StriverDSA/subsetSum.cpp
+++ StriverDSA/subsetSum.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 vector<int> glob;
+vector<vector<int>> globb;
 
 void subsetSumsTemp(vector<int>& nums,int index ,int sum) {
 
@@ -18,14 +20,36 @@ void subsetSumsTemp(vector<int>& nums,int index ,int sum) {
     return;
 
 }
-
+class Solution {
+public:
+    vector<vector<int>> glob;
+    void subsetsTemp(vector<int> nums , int index ,vector<int> temp){
+        glob.push_back(temp);
+        for(int i = index ;i < nums.size() ;i++){
+            if(i > index && nums[i] == nums[i-1]) continue;
+            temp.push_back(nums[i]);
+            subsetsTemp(nums,i+1,temp);
+            temp.pop_back();
+        }
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        vector<int> temp;
+        subsetsTemp(nums,0,temp);
+        return glob;
+    }
+};
 
 int main(){
-    vector<int> nums = {2,3};
+    vector<int> nums = {1,2,2};
+    sort(nums.begin(),nums.end());
     vector<int> temp;
-    subsetSumsTemp(nums,0,0);
+    subsetTemp(nums,0,temp);
     sort(glob.begin(),glob.end());
-    for(auto vec:glob){
-        cout << vec;
+    for(auto vec:globb){
+        for(int num:vec){
+            cout << num << " ";
+        }
+        cout << endl;
     }
 }
