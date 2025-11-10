f68b2d3b3b13b42e1349c072d5161dc3791e2ac0
Garv Thakral <garvthakralfx@gmail.com>
2025-11-11 00:26:58 +0530

    min bit flips to reach goal

---
+++ StriverDSA/minBitFlips.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minBitFlips(int start, int goal) {
+        int count = 0;
+        int xored = start^goal;
+        for(int i = 0 ; i < 32 ; i++){
+            if((xored & 1 )== 1){
+                count ++;
+            }
+            xored >>= 1;
+        }
+        return count;
+    }
+};
