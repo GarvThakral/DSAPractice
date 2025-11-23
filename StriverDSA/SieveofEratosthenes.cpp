4ac81eb61ab0290b01f5bca23f51ba5f08d6cbc5
Garv Thakral <garvthakralfx@gmail.com>
2025-11-23 22:37:34 +0530

    Sieve of Eratosthenes

--- StriverDSA/SieveofEratosthenes.cpp
+++ StriverDSA/SieveofEratosthenes.cpp
@@ -27,6 +27,43 @@ int sieveOf(int n){
     return sum;
 }
 
+vector<vector<int>> PrimeFactorisationQuery(vector<int> queries){
+    vector<vector<int>> answers;
+    int biggest = 0;
+    for(int i = 0 ;i < queries.size() ;i++){
+        biggest = max(biggest,queries[i]);
+    }
+    vector<int> arr(biggest+1,0);
+    for(int i = 2 ; i < arr.size() ; i++){
+        arr[i] = i;
+    }
+    for(int i = 2; i < arr.size() ; i++){
+        if(arr[i] == i){
+            for(int j = (i*i) ; j < arr.size() ;j+=i){
+                if(arr[j] > i){
+                    arr[j] = i;
+                }
+            }
+        }
+    }
+    for(auto query:queries){
+        vector<int> temp;
+        while(query > 1){
+            temp.push_back(arr[query]);
+            query/=arr[query];
+        }
+        answers.push_back(temp);
+    }
+    return answers;
+}
+
 int main(){
-    sieveOf(31);
+    vector<int> queries = {2, 3, 4, 5, 6};
+    vector<vector<int>> answer =  PrimeFactorisationQuery(queries);
+    for(auto ans:answer){
+        for(int it:ans){
+            cout << it << ' ';
+        }
+        cout << endl;
+    }
 }
