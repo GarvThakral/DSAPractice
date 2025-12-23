bcaae596ed656a2557e663bb101678b5640d60d8
Garv Thakral <garvthakralfx@gmail.com>
2025-12-23 09:34:25 +0530

    trapping rainwater

--- StriverDSA/trapping_rainwater.cpp
+++ StriverDSA/trapping_rainwater.cpp
@@ -1,23 +1,41 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 #include <stack>
 using namespace std;
 
-int trapping_water(vector<int> nums){
-    stack<int> s1;
-    for(int i:nums){
-        if(s1.empty()){
-            s1.push(i);
-        }else if(s1.top() >= i){
-            s1.push(i);
-        }else{
-            int leftMax = 0;
-            int leftmid = 0;
-            while(!s1.empty() && s1.top() < i){
+vector<int> prefixMax(vector<int> nums){
+    vector<int> answer;
+    answer.push_back(nums[0]);
+    int maxSoFar = nums[0];
+    for(int i = 1 ; i < nums.size() ;i++){
+        maxSoFar = max(maxSoFar,nums[i]);
+        answer.push_back(maxSoFar);
+    }
+    return answer;
+}
+vector<int> suffixMax(vector<int> nums){
+    vector<int> answer;
+    answer.push_back(nums[nums.size()-1]);
+    int maxSoFar = nums[nums.size()-1];
+    for(int i = nums.size()-2 ; i >= 0 ;i--){
+        maxSoFar = max(maxSoFar,nums[i]);
+        answer.push_back(maxSoFar);
+    }
+    reverse(answer.begin(),answer.end());
+    return answer;
+}
 
-            }
+int trapping_water(vector<int> nums){
+    vector<int> pref = prefixMax(nums);
+    vector<int> suff = suffixMax(nums);
+    int total =0;
+    for(int i = 0 ; i < nums.size() ;i++){
+        if(nums[i] < pref[i] && nums[i] < suff[i]){
+            total += min(pref[i],suff[i]) - nums[i];
         }
     }
+    return total;
 }
 
 
