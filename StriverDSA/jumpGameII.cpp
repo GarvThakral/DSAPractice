58bb4769d171c75b14cb66871991c0d7c374921b
Garv Thakral <garvthakralfx@gmail.com>
2026-01-13 20:15:11 +0530

    Jump game leet optimized

--- StriverDSA/jumpGameII.cpp
+++ StriverDSA/jumpGameII.cpp
@@ -7,21 +7,51 @@ using namespace std;
 
 int minJumpsGLob = INT_MAX;
 
-void minJumps(vector<int> nums,int currIndex,int numJumps){
-    if(currIndex >= nums.size()-1){
-        minJumpsGLob = min(minJumpsGLob,numJumps);
-        return;
+int minJumps(vector<int> nums){
+    int leftRange = 0;
+    int rightRange = 0;
+    int tempRight = 0;
+    int numJumps = 0;
+    for(int i = 0 ; i < nums.size() ;i++){
+        if(rightRange>=nums.size()-1) return numJumps;
+        numJumps++;
+        for(int j = leftRange ; j <= rightRange ; j++){
+            tempRight = max(tempRight,nums[j]);
+        }
+        leftRange=rightRange+1;
+        rightRange+=tempRight;
     }
-    if(nums[currIndex]==0){
-        return;
-    }
-    for(int i = 1 ; i <= nums[currIndex] ; i++){
-        minJumps(nums,currIndex+i,numJumps+1);
-    }   
+    return numJumps;
 }
 
 int main(){
     vector<int> nums = {2,3,0,1,4};
-    minJumps(nums,0,0);
-    cout << minJumpsGLob;
+    int ans = minJumps(nums);
+    cout << ans;
 }
+
+class Solution {
+public:
+int minJumpsGLob = INT_MAX;
+
+int minJumps(vector<int> nums){
+    int leftRange = 0;
+    int rightRange = 0;
+    int tempRight = 0;
+    int numJumps = 0;
+    for(int i = 0 ; i < nums.size() ;i++){
+        int tempRight = rightRange;  
+        if(rightRange>=nums.size()-1) return numJumps;
+        numJumps++;
+        for(int j = leftRange ; j <= rightRange ; j++){
+            tempRight = max(tempRight,j+nums[j]);
+        }
+        leftRange=rightRange+1;
+        rightRange=tempRight;
+    }
+    return numJumps;
+}
+int jump(vector<int>& nums) {
+    return minJumps(nums);
+}
+};
